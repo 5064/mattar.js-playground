e0fdddfb639c767f7a9e64b582346d1aa30dae07
5064 <5og4.kirin@gmail.com>
2020-07-14 18:43:57 +0900

    wip!

--- galton-board/sketch.js
+++ galton-board/sketch.js
@@ -17,7 +17,7 @@ const GaltonBoard = (p5) => {
     const START_X = CANVAS_X / 2
     const START_Y = 150;
     const PARTICLE_R = 3
-    const OBS_R = 4
+    const OBS_R = 5
 
     p5.setup = () => {
         p5.createCanvas(CANVAS_X, CANVAS_Y);
@@ -28,7 +28,7 @@ const GaltonBoard = (p5) => {
         world.gravity.y = 2;
 
         for (let i = 0; i < MAX_PARTICLES; i++) {
-            const p = new Particle(p5, START_X + (Math.random() * 10), START_Y / 3 + (Math.random() * 10), PARTICLE_R);
+            const p = new Particle(p5, START_X + (Math.random() * 10), START_Y / 2 + (Math.random() * 10), PARTICLE_R);
             Matter.World.add(world, p.body)
             particles.push(p);
         }
@@ -49,8 +49,8 @@ const GaltonBoard = (p5) => {
                 shiftX += Obstacle.SPACING / 2  // Partricleが垂直落下したとき中心に
             }
             for (let j = 0; j < i + 3; j++) {
-                shiftX += calcIntervalX(j, PARTICLE_R + OBS_R)
-                const shiftY = perpendicular(i, Obstacle.SPACING);
+                shiftX += calcIntervalX(j, PARTICLE_R * 2 + OBS_R * 2);
+                const shiftY = perpendicular(i, PARTICLE_R * 2 + OBS_R * 2);
 
                 const o = new Obstacle(p5, START_X + shiftX, (START_Y + 20) + shiftY, OBS_R);
                 Matter.World.add(world, o.body)
@@ -74,8 +74,8 @@ const GaltonBoard = (p5) => {
     }
 
     setupStorage = () => {
-        const sLeft = new PStorage(p5, 0, 0, 0, START_Y, START_X - PARTICLE_R / 2, START_Y);
-        const sRight = new PStorage(p5, CANVAS_X, 0, CANVAS_X, START_Y, START_X + PARTICLE_R / 2, START_Y);
+        const sLeft = new PStorage(p5, 0, 0, 0, START_Y, START_X - PARTICLE_R, START_Y, START_X - PARTICLE_R, START_Y - 10);
+        const sRight = new PStorage(p5, CANVAS_X, 0, CANVAS_X, START_Y, START_X + PARTICLE_R, START_Y, START_X + PARTICLE_R, START_Y - 10);
         Matter.World.add(world, sLeft.body)
         Matter.World.add(world, sRight.body)
         storages.push(...[sLeft, sRight])
@@ -86,7 +86,7 @@ const GaltonBoard = (p5) => {
         const bTop = new Boundary(p5, CANVAS_X / 2, 0 + bT / 2, CANVAS_X, bT);
         const bLeft = new Boundary(p5, 0 + bT / 2, CANVAS_Y / 2, bT, CANVAS_Y);
         const bRight = new Boundary(p5, CANVAS_X - bT / 2, CANVAS_Y / 2, bT, CANVAS_Y);
-        const bBottom = new Boundary(p5, CANVAS_X / 2, CANVAS_Y - bT / 2, CANVAS_X, bT);
+        const bBottom = new Boundary(p5, CANVAS_X / 2, CANVAS_Y - bT / 2, CANVAS_X, 10);
         Matter.World.add(world, bTop.body);
         Matter.World.add(world, bLeft.body);
         Matter.World.add(world, bRight.body);
